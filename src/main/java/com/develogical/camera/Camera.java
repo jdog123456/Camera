9287088c161f97812bc347290e9c67c983312800
James Sarkar <j.sarkar@sap.com>
2021-03-16 12:10:26 -0400

    First broken test

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -1,6 +1,11 @@
 package com.develogical.camera;
 
 public class Camera {
+    private final Sensor sensor;
+
+    public Camera(final Sensor sensor) {
+        this.sensor = sensor;
+    }
 
     public void pressShutter() {
         // not implemented
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -2,12 +2,18 @@ package com.develogical.camera;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        final Sensor sensor = Mockito.mock(Sensor.class);
+        final Camera cam = new Camera(sensor);
+
+        cam.powerOn();
+
+        verify(sensor, times(1)).powerUp();
     }
 }
